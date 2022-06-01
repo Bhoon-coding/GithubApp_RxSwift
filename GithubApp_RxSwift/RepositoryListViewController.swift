7237a7dfcc6b205bc6379fe9a6b60aea0c730b00
bhoon-coding <bhooncoding@gmail.com>
2022-06-01 14:48:23 +0900

    기본 UI 구성

--- GithubApp_RxSwift/RepositoryListViewController.swift
+++ GithubApp_RxSwift/RepositoryListViewController.swift
@@ -8,5 +8,47 @@
 import UIKit
 
 class RepositoryListViewController: UITableViewController {
- 
+    // MARK: - Properies
+    
+    private let organization = "Apple"
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = organization + "Repositories"
+        
+        // MARK: - TableView 를 당겨서 새로고침
+        self.refreshControl = UIRefreshControl()
+        let refreshControl = self.refreshControl!
+        refreshControl.backgroundColor = .white
+        refreshControl.tintColor = .darkGray
+        refreshControl.attributedTitle = NSAttributedString(string: "당겨서 새로고침")
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        
+        tableView.register(RepositoryListCell.self, forCellReuseIdentifier: "RepositoryListCell")
+        tableView.rowHeight = 140
+    }
+    
+    // MARK: - @objc
+    @objc func refresh() {
+        
+    }
+    
+}
+
+// MARK: - extension
+
+// MARK: - UITableView Delegate, DataSource
+extension RepositoryListViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RepositoryListCell", for: indexPath) as? RepositoryListCell else { return UITableViewCell() }
+        
+        return cell
+    }
 }
